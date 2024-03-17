d0ec91ed5d869d3e47b27c5ce57c5160ebf0bb51
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-03-17 14:11:01 +0300

    adding objects

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let employee = {};
+employee.Name = 'Joshua';
+employee.streetAdress = 'Miotoni ln';
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updateemployee = { ...employee };
+    updateemployee[key] = value;
+
+    return updateemployee;
+    // Code to return new, updated menu object goes here
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value; //Why are we using bracket notation here?
+
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const updateemployee = { ...employee};
+    delete updateemployee[key];
+    return updateemployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
